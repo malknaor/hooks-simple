1e1dd72742ff3899ca388be64efc56c75f4d660a
malknaor <malknaor@gmail.com>
2019-08-20 13:53:59 +0300

    Final commit - Refuctured ResourcesList to use useRespurces And adde UserList component

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
 import ResourceList from './ResourceList';
+import UserList from './UserList';
 
 const App = () => {
     const [resource, setResource] = useState('posts');
 
     return (
-        <div>
+        <div className="ui container">
+            <UserList />
             <div>
-                <button onClick={() => setResource('posts')}>
+                <button className="ui button primary" onClick={() => setResource('posts')}>
                     Posts
                 </button>
-                <button onClick={() => setResource('todos')}>
+                <button className="ui button primary" onClick={() => setResource('todos')}>
                     Todos
                 </button>
             </div>
--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,23 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
+import useResources from './useResources';
 
 const ResourceList = ({ resource }) => {
-    const [resources, setResources] = useState([]);
-
-
-    useEffect(() => {
-        (async () => {
-            const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
-
-            setResources(response.data);
-        })(resource);
-    }, [resource]);
+    const resources = useResources(resource);
 
     return (
-        <div>
-            {resources.length}
-        </div>
+        <ul>
+            {resources.map((record) =>
+                <li key={record.id}>
+                    {record.title}
+                </li>
+            )}
+        </ul>
     );
-}
+};
 
 export default ResourceList;
